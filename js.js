403285f6edf431ceb4e139604ff698f52f2cdaa2
chaimaehassari <chaimaehassari1@gmail.com>
2024-12-04 20:11:57 +0100

    2

--- js.js
+++ js.js
@@ -1,34 +1,40 @@
-
-        let cart = {};
-        let total = 0;
-
-        function addToCart(item, price) {
-            if (cart[item]) {
-                cart[item].quantity += 1;
-            } else {
-                cart[item] = { price: price, quantity: 1 };
-            }
-            updateCart();
+    class  ShoppingCart {
+        // constructor (this permet d'accéder aux propriétés et méthodes de l'objet courant)
+        constructor() {
+            this.cart={};
+            this.total=0
         }
-
-        function updateCart() {
+        // les methodes de la class ShoppingCart
+         addToCart(item, price) {
+            if (this.cart[item]) {
+                this.cart[item].quantity += 1;
+            } else {
+                this.cart[item] = { price: price, quantity: 1 };
+            }
+            this.updateCart();
+        }
+        updateCart() {
             const cartDiv = document.getElementById('cart');
             cartDiv.innerHTML = '';
-            total = 0;
-
-            for (const item in cart) {
-                const itemTotal = cart[item].price * cart[item].quantity;
-                total += itemTotal;
-                cartDiv.innerHTML += `<div class="flex justify-between">
-                    <span>${item} x ${cart[item].quantity}</span>
-                    <span>$${itemTotal}</span>
-                </div>`;
+            this.total = 0;
+            for (const item in this.cart) {
+            const itemTotal = this.cart[item].price * this.cart[item].quantity;
+            this.total += itemTotal;
+             cartDiv.innerHTML += `<div class="flex justify-between">
+                                <span>${item} x ${this.cart[item].quantity}</span>
+                                <span>$${itemTotal}</span>
+                            </div>`;
+                            }   
+            
+            document.getElementById('total').innerText = `Total: $${this.total}`;
+            }
+        proceedToCheckout() {
+                alert('Proceeding to checkout with total: $' + this.total);
             }
 
-            document.getElementById('total').innerText = `Total: $${total}`;
-        }
+    }
 
-        function proceedToCheckout() {
-            alert('Proceeding to checkout with total: $' + total);
-        }
+
+
+
     
